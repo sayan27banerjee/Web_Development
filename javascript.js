8f15cee0dbc781390879704981d68f0f0281fd8f
sayan27banerjee <sayanbanerjee1010@gmail.com>
2023-02-25 19:54:35 +0530

    Javascript

---
+++ javascript.js
@@ -0,0 +1,26 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+let a = 16+ 4 + "Volvo";
+let f = "volvo" + 16 + 4
+console.log(a)
+console.log(f)
+let ab = 123e5
+console.log(ab)
